72caf54726b723a1a3aa8d4060ae2616710bee56
2016122458 <44885466+2016122458@users.noreply.github.com>
2020-02-03 13:23:07 +0800

    commit

---
+++ src/main/java/com/autosendemail/email/EmailApplication.java
@@ -0,0 +1,63 @@
+package com.autosendemail.email;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.CommandLineRunner;
+
+@SpringBootApplication
+public class EmailApplication implements CommandLineRunner {
+
+    @Autowired
+    private EmailDbOperate emailDbOperateForSend;
+
+    @Autowired
+    private EmailAppRunableForVeriEmail emailAppRunableForVeriEmail;
+
+    @Autowired
+    private EmailSendGridHandle emailSendGridHandle;
+
+    @Autowired
+    private EmailMailGunHandle emailMailGunHandle;
+
+    @Autowired
+    private EmailSparkPostHandle emailSparkPostHandle;
+
+    @Autowired
+    private EmailBatchInfoService emailBatchInfoService;
+
+    @Autowired
+    private EmailSendSourceControlRepository emailSendSourceControlRepository;
+
+    @Autowired
+    private EmailAutoSendConfigRepository emailAutoSendConfigRepository;
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmailApplication.class, args);
+    }
+
+    @Override
+    public void run(String... strings) throws Exception {
+
+        EmailAppRunableForCloudEmailBatch emailAppRunableForCloudEmailBatchSendGrid =
+                new EmailAppRunableForCloudEmailBatch(emailDbOperateForSend,emailBatchInfoService,
+                        emailSendSourceControlRepository,emailAutoSendConfigRepository, EmailSendServerType.SENDGRID);
+        Thread thread_sendMessageBatchSendGrid = new Thread(emailAppRunableForCloudEmailBatchSendGrid);
+        thread_sendMessageBatchSendGrid.start();
+        System.out.println("thread_sendMessageBatch=" +  thread_sendMessageBatchSendGrid.getPriority());
+
+//        Thread.sleep(10*1000);
+//
+//        EmailAppRunableForCloudEmailBatch emailAppRunableForCloudEmailBatchMailGun =
+//                new EmailAppRunableForCloudEmailBatch(emailDbOperateForSend,emailBatchInfoService,
+//                        emailSendSourceControlRepository,emailAutoSendConfigRepository, EmailSendServerType.MAILGUN);
+//        Thread thread_sendMessageBatchMailGun = new Thread(emailAppRunableForCloudEmailBatchMailGun);
+//        thread_sendMessageBatchMailGun.start();
+//        System.out.println("thread_sendMessageBatchMailGun=" +  thread_sendMessageBatchMailGun.getPriority());
+
+
+
+
+    }
+}
+
